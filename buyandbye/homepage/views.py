3afbe49a62a5ffcdfb238adade9e25a3da7e07da
Sambeg Shrestha <sthasambeg2@outlook.com>
2019-12-27 09:17:59 +0545

    Items Page Creation and Functionality, Updated Home Layout, User Profile Functionality
    1. Created a Detailed individual page for each item post and added functionality like update existing item post, delete the post based on whether the user is authenticated or not.
    
    2. Tried different HTML and CSS update the homepage item post layouts (Card), Added a non-functioning search bar in Nav bar (needs UI working as well).
    
    3. Updated the User app with profile update form so now users can update their profile when they are authenticated. (Note: Password change still not available, it is a work in progress)

--- buyandbye/homepage/views.py
+++ buyandbye/homepage/views.py
@@ -1,25 +1,61 @@
 from django.shortcuts import render
 from .models import Items
-
-#posts=[
-#    {
-#        'author' : 'Sambeg',
-#        'title': 'Blog Post',
-#        'content': '1st post content'
-#    },
-# {
-#      'author': 'Sambeg',
-#      'title': 'Blog Post',
-#       'content': '2nd post content'
-#   }
-#
-#]
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
 def home(request):
     context={
         'item': Items.objects.all()
     }
     return render(request, 'homepage/home.html', context)
+
+class ItemListView(ListView):
+    model = Items
+    template_name = 'homepage/home.html' #app/model_viewtype.html
+    context_object_name = 'item'
+    ordering = ['-date_posted']
+
+class ItemDetailView(DetailView):
+    model = Items
+
+class ItemCreateView(LoginRequiredMixin, CreateView):
+    model = Items
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class ItemUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Items
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class ItemDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Items
+    success_url='/'
+
+    def test_func(self):
+        item = self.get_object()
+        if self.request.user == item.author:
+            return True
+        return False
+
 
 def aboutus(request):
     return render(request, 'homepage/about.html', {'title': 'About'})
--- buyandbye/users/forms.py
+++ buyandbye/users/forms.py
@@ -2,11 +2,25 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from phonenumber_field.formfields import PhoneNumberField
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    phone = PhoneNumberField()
+    phone = PhoneNumberField(required=False)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'phone', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    phone = PhoneNumberField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
