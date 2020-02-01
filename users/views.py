bb7b7a5c66cebbaace45947940e39159bbbc8dd4
Sambeg Shrestha <sthasambeg2@outlook.com>
2020-02-01 22:51:10 +0545

    Minor modificatios
    .gitignore:
    1. added vscode folder
    
    db
    
    homepage:
    1. CSS -  added 'btn' css rounded, hover-effect
    2. HTML - base: added fontawesome;
    
    users:
    1. HTML - login, logout, register: modified design and layout
    
    cache

--- users/views.py
+++ users/views.py
@@ -69,7 +69,7 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        login(request, user)
+        login(request, user, backend='django.contrib.auth.backends.ModelBackend')
         messages.success(
             request, f'Your email has been validated! Now, you can login.')
         return redirect('login')
