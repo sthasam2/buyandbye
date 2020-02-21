749e5b82b3f0bf3937ed587c9341b9b8db554ef5
Sambeg Shrestha <sthasambeg2@outlook.com>
2020-02-21 11:44:23 +0545

    Python script
    -Python script for auto populating Category, Subcategory (error while importing module)

--- buyandbye/command.py
+++ buyandbye/command.py
@@ -1,10 +1,18 @@
 import json
-from homepage.models import Category
+from homepage.models import Category, SubCategory, Item
 
-with open('category.json') as f:
+with open('.json/category.json') as f:
     template = json.load(f)
 
-    for newCategory in template:
-        newCategory = Category(
-            name=newCategory['name'], slug=newCategory['slug'])
-        newCategory.save()
+for newCategory in template:
+    newCategory = Category(
+        name=newCategory['name'], slug=newCategory['slug'])
+    newCategory.save()
+
+with open('.json/subcat.json') as f:
+    template2 = json.load(f)
+
+for new in template2:
+    new = SubCategory(parent_category=Category.objects.get(
+        name=new['parent_name']), subname=new['subname'])
+    new.save()
