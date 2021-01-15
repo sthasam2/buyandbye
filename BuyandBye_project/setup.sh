2461e0bd07f1d7f66e8993735d3096959dc020d1
Sambeg Shrestha <sthasam2@gmail.com>
2021-01-15 14:27:25 +0545

    Updated Bash script

--- BuyandBye_project/setup.sh
+++ BuyandBye_project/setup.sh
@@ -1,24 +1,27 @@
 CURR=`pwd`
 VENV=".venv/"
 
-echo "\e[1;92mSetup BuyandBye\e[0m"
 echo "---------------"
-echo "requirements: Python3, Pip, Virtualenv"
+echo "\e[1;92mBuyandBye Setup\e[0m"
+echo "---------------\n"
+
+echo "\e[1;4mREQUIREMENTS:\e[0m\n1. Python3 \n2. Pip \n3. Virtualenv"
 echo " "
 
 if [ -d "$CURR/$VENV" ]; then
-    echo "Requirements satisfied!"
+    echo "Virtual environment already available!"
 else
-    echo "Creating virtual environment in folder \e[1m.venv\e[0m..."
+    echo "Creating virtual environment in folder \e[1m.venv\e[0m...\n"
     virtualenv .venv
     
-    echo "Installing Requirements...\n"
+    echo "\n\e[1;92mInstalling Dependencies...\e[0m\n"
     ./.venv/bin/pip install -r requirements.txt
-    ./.venv/bin/python manage.py makemigrations
-    ./.venv/bin/python manage.py migrate
+    echo " "
+    ./.venv/bin/python manage.py makemigrations    
+    echo " "
+    ./.venv/bin/python manage.py migrate    
 fi
 
-# activate() {. .venv/bin/activate}
-# echo "Hello"
+echo "\e[1;92mSETUP COMPLETE!"
 
 
